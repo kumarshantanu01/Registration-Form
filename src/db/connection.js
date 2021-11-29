65558b8224aa15079894fca07929a5393cd10a41
kumarshantanu01 <shantanuk280@gmail.com>
2021-11-29 19:59:31 +0530

    commit3

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/registration-form"
+mongoose.connect("mongodb://localhost:27017/registrationform"
 ).then(() =>{
     console.log(`connection successful`);
 }).catch((e) => {
